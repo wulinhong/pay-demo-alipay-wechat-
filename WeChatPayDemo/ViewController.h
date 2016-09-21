c22215dc46edc01b8421b3f171e863eb22b49281
lh_w <lh_w@lh-wdeMacintosh.local>
2016-09-21 10:27:20 +0800

    微信支付demo

---
+++ WeChatPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatPayDemo
+//
+//  Created by lh_w on 15/12/28.
+//  Copyright © 2015年 lh_w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
