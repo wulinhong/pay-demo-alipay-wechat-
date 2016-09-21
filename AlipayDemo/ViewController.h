8aad415dd2c576643d9e639eb176f056f171a01e
lh_w <lh_w@lh-wdeMacintosh.local>
2016-09-21 10:22:59 +0800

    支付宝支付的demo

---
+++ AlipayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlipayDemo
+//
+//  Created by lh_w on 15/12/23.
+//  Copyright © 2015年 V1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlipayDemo/model/ShopInformation.m
@@ -0,0 +1,13 @@
+//
+//  ShopInformation.m
+//  AlipayDemo
+//
+//  Created by lh_w on 15/12/23.
+//  Copyright © 2015年 V1. All rights reserved.
+//
+
+#import "ShopInformation.h"
+
+@implementation ShopInformation
+
+@end
